dd3a04ce791d89cb563c99204e05a560fdbd23e9
Ihuoma Alozie <alozie.ihuomao@gmail.com>
2022-04-03 04:29:44 +0100

    challenge-2 changes

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -1,11 +1,16 @@
-pragma solidity 0.8.4;
 // SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 // learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
 
-contract YourToken is ERC20 {
+contract YourToken is ERC20, Ownable  {
     constructor() ERC20("Gold", "GLD") {
-        //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
+        _mint(msg.sender, 1000 * 10 ** 18);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
